eafdd0295c7b86db2ef4a8cf68985cc123fd0405
Hotdogwarrier21 <pierce.keene@gmail.com>
2024-08-15 15:02:37 +0000

    aded

---
+++ src/printing.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cout << "Hello Mr. Foster!" << endl;
+    cout << "My name is Pierce ";
+    cout << "and I am in 11th grade" << endl;
+    cout << "Whoah, how is this one print statement \n" << "on two lines?" << endl;
+}
