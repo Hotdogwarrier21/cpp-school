eb6694da1e9e9c4df1d409565751b1dc79b51262
Hotdogwarrier21 <pierce.keene@gmail.com>
2024-08-16 12:21:27 +0000

    i changed things

--- src/printing.cpp
+++ src/printing.cpp
@@ -7,4 +7,5 @@ int main(){
     cout << "My name is Pierce ";
     cout << "and I am in 11th grade" << endl;
     cout << "Whoah, how is this one print statement \n" << "on two lines?" << endl;
+    cout << "I am " <<  (10+7) << " years old." << endl;
 }
