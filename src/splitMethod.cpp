988cc45b9b869c57235a9412d2119e2b0ee980f9
Hotdogwarrier21 <pierce.keene@gmail.com>
2024-09-17 14:20:54 +0000

    WE HAVE SPLIT

---
+++ src/splitMethod.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split(string s, string delim){
+    vector<string> tokens; // stores all the words in s
+    
+    int index = s.find(delim);
+    while (index != string::npos){
+        string token = s.substr(0,index);
+        tokens.push_back(token);
+        s=s.substr(index+1);
+        index = s.find(delim);
+    }
+    tokens.push_back(s);
+    return tokens;
+}
+
+int main(){
+
+    int runs;
+    cin >> runs;
+    string burn;
+    getline(cin, burn);
+    for (int i = 0; i < runs; i++){
+
+        
+
+    }
+}
